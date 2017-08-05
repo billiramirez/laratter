1a6d0c0d92c3f3c2ba6358772b8d4bd6657cadaa
Billi Ramirez <ramirezbilly42@outlook.com>
2017-08-05 14:38:39 -0600

    Running Migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+//        Add this code in order to avoid an error in the migrate operation
+            Schema::defaultStringLength(191);
     }
 
     /**
