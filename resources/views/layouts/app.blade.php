a7657287ac57fdb769910fdaa52cd01d9fab8b9d
Billi Ramirez <ramirezbilly42@outlook.com>
2017-09-11 13:04:26 -0600

    adding the frontend of the responses with vue.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,7 +81,7 @@
    <script  src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
-    {{--  <script src="{{ mix('/js/jquery.min.js') }}"></script>  --}}
+    {{--  <script src="{{ mix('/js/jquery.min.js') }}"></script>    --}}
 
 
 </body>
--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -5,4 +5,6 @@
     <h1 class="h3">Mensaje id: {{ $message->id }}</h1>
     @include('messages.message')
 
+    <responses :message="{{ $message->id }}"></responses>
+
 @endsection
