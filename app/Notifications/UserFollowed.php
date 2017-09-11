dac908a7458bca65ab05ce3fdf2ab8fda38bcade
Billi Ramirez <ramirezbilly42@outlook.com>
2017-09-11 14:29:05 -0600

    saving our notifications in the database

--- app/Notifications/UserFollowed.php
+++ app/Notifications/UserFollowed.php
@@ -31,7 +31,7 @@ class UserFollowed extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail', 'database'];
     }
 
     /**
@@ -59,7 +59,7 @@ class UserFollowed extends Notification
     public function toArray($notifiable)
     {
         return [
-            //
+            'follower' => $this->follower,
         ];
     }
 }
